ceeb016654b68ed68a1ab2afeadca3c655237ca4
manh <luongvanmanh1929@gmail.com>
2022-04-24 09:40:11 +0700

    add link giao dien xong

--- routes/shop_Router.js
+++ routes/shop_Router.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     res.render('shop_home')
 })
 router.get('/gioi-thieu', (req, res) => {
-    res.render('shop_gioithieu')
+    res.render('shop_gioi_thieu')
 })
 router.get('/huong-dan-mua-hang', (req, res) => {
     res.render('shop_huong_dan_mua_hang')
