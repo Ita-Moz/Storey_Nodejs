3de7604cf8ee444d71c04754e4ea14366b4e757f
manh <luongvanmanh1929@gmail.com>
2022-04-27 22:52:27 +0700

    toi uu check dang ky

--- public/js/dangnhap.js
+++ public/js/dangnhap.js
@@ -1,11 +1,5 @@
 $(document).ready(function () {
-    $('#btn_dangky').on('click', () => {
-      
-          console.log('SignUp Success!!!')
-          alert('Đăng kí thành công <3');
-          
-    });
-  
+
     const signUpButton = document.getElementById("signUp");
     const signInButton = document.getElementById("signIn");
     const container = document.getElementById("container");
